caf4b310ac9f200b5142969aeb1a05746a4ffdb0
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-01-30 15:06:59 +0300

    INITIALIZED folders and files for MARKET

---
+++ lib/features/market/ui/market_product_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MarketProductScreen extends StatefulWidget {
+  const MarketProductScreen({super.key});
+
+  @override
+  State<MarketProductScreen> createState() => _MarketProductScreenState();
+}
+
+class _MarketProductScreenState extends State<MarketProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
