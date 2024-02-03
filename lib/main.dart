7cb86790f5572d29546d332c096a92f8fc6e96e8
Jason1258t <ivandomogatskij@gmail.com>
2024-02-03 16:22:52 +0300

    эщ эщ эщ, и оно запустилось

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:bcsports_mobile/app.dart';
-import 'package:bcsports_mobile/providers.dart';
+import 'package:bcsports_mobile/services/custom_bloc_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = CustomBlocObserver();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
