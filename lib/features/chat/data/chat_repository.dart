91db1e724534ea53c974e274fd6d2016daf13534
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-02-10 22:33:03 +0300

    ADDED -func to filter user list

--- lib/features/chat/data/chat_repository.dart
+++ lib/features/chat/data/chat_repository.dart
@@ -2,6 +2,11 @@ import 'package:bcsports_mobile/features/social/data/models/user_model.dart';
 
 class ChatRepository {
   final List<UserModel> socailUserList = [];
+   List<UserModel> filteredUserList = [];
 
-  void filterUserByInputText(String name) async {}
+  void filterUserByInputText(String name) {
+    filteredUserList = socailUserList
+        .where((user) => (user.displayName ?? "").startsWith(name))
+        .toList();
+  }
 }
