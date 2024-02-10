923fd1792a997acc47babe4eabc20da0541006ae
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-02-10 22:29:49 +0300

    to dima you

--- lib/features/chat/data/chat_repository.dart
+++ lib/features/chat/data/chat_repository.dart
@@ -1,3 +1,7 @@
-class ChatRepository{
-  
+import 'package:bcsports_mobile/features/social/data/models/user_model.dart';
+
+class ChatRepository {
+  final List<UserModel> socailUserList = [];
+
+  void filterUserByInputText(String name) async {}
 }
